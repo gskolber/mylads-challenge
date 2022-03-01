7c91eed8159642ddab32a83854a7c79d448b9d0f
Gabriel Kolber <gabrielkolber@icloud.com>
2022-03-01 01:51:36 -0300

    move some business logic to player schema
    use put_assoc approach to import

--- challenge_mylads/lib/challenge_mylads/player.ex
+++ challenge_mylads/lib/challenge_mylads/player.ex
@@ -2,27 +2,61 @@ defmodule ChallengeMylads.Player do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias ChallengeMylads.Player.{ClubSeniors, ClubYouthHonours, NationalTeamSeniors,
+  NationalTeamYouthHonours, TotalSeniors, CareerStats}
+
   schema "player" do
     field :club_id, :integer
     field :player_id, :integer
 
-    has_many :career_stats, ChallengeMylads.Player.CareerStats
-    has_one :club_senior, ChallengeMylads.Player.ClubSeniors
-    has_one :club_youth_honours, ChallengeMylads.Player.ClubYouthHonours
-    has_one :national_teams_seniors, ChallengeMylads.Player.NationalTeamSeniors
-    has_one :national_team_youth_honours, ChallengeMylads.Player.NationalTeamYouthHonours
-    has_one :total_seniors, ChallengeMylads.Player.TotalSeniors
+    has_many :career_stats, CareerStats
+    has_one :club_senior, ClubSeniors
+    has_one :club_youth_honours, ClubYouthHonours
+    has_one :national_teams_seniors, NationalTeamSeniors
+    has_one :national_team_youth_honours, NationalTeamYouthHonours
+    has_one :total_seniors, TotalSeniors
     timestamps()
   end
 
   @doc false
   def changeset(player, attrs) do
+    attrs = Map.merge(attrs, normalize_attrs(attrs))
     player
     |> cast(attrs, [:club_id, :player_id])
+    |> cast_assoc(:club_senior, with: &ClubSeniors.changeset/2)
+    |> cast_assoc(:club_youth_honours, with: &ClubYouthHonours.changeset/2)
+    |> cast_assoc(:national_teams_seniors, with: &NationalTeamSeniors.changeset/2)
+    |> cast_assoc(:national_team_youth_honours, with: &NationalTeamYouthHonours.changeset/2)
+    |> cast_assoc(:total_seniors, with: &TotalSeniors.changeset/2)
+    |> cast_assoc(:career_stats, with: &CareerStats.changeset/2)
   end
 
-  def add_stats(player, attrs) do
-    player
-    |> cast(attrs, [:carrer_stats, :club_senior ,:club_youth_honours, :national_teams_seniors,:national_team_youth_honours])
+  def normalize_attrs(attrs) do
+
+    club_senior = normalize_map(attrs["club_seniors"])
+    club_youth_honours = normalize_map(attrs["club_youth_honours"])
+    national_teams_seniors = normalize_map(attrs["national_teams_seniors"])
+    national_team_youth_honours = normalize_map(attrs["national_team_youth_honours"])
+    total_seniors = normalize_map(attrs["\uFEFFtotal_seniors"])
+    career_stats = normalize_map(attrs["career_stats"])
+
+    %{
+      "club_senior" => club_senior,
+      "club_youth_honours" => club_youth_honours,
+      "national_teams_seniors" => national_teams_seniors,
+      "national_team_youth_honours" => national_team_youth_honours,
+      "total_seniors" => total_seniors,
+      "career_stats" => career_stats
+    }
+
+  end
+
+  def normalize_map (map) do
+    {:ok, new_map} = map
+    |> String.replace("=>", ":")
+    |> Jason.decode()
+
+    IO.inspect(new_map)
+    new_map
   end
 end
